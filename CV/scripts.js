61198cf5c79e040ec6021b37ea6ff4e968fa8c26
Andres Almeida <asalmeida4@espe.edu.ec>
2023-11-16 21:00:38 -0500

    Se añade la sección contactos

--- CV/scripts.js
+++ CV/scripts.js
@@ -12,4 +12,33 @@ document.querySelectorAll('nav li').forEach(navItem => {
       });
     });
   });
+
+
+
+
+  // PARA LA SECCION CONTACTOS
+
+document.querySelectorAll('.email p').forEach(emailElement => {
+    emailElement.addEventListener('click', function() {
+      const emailText = this.innerText.trim();
+  
+      // Create a temporary input element
+      const input = document.createElement('input');
+      input.setAttribute('value', emailText);
+      document.body.appendChild(input);
+  
+      // Select the text in the input
+      input.select();
+      input.setSelectionRange(0, 99999); // For mobile devices
+  
+      // Copy the text to the clipboard
+      document.execCommand('copy');
+  
+      // Remove the temporary input
+      document.body.removeChild(input);
+  
+      // Inform the user
+      alert(`¡${emailText} ha sido copiado al portapapeles!`);
+    });
+  });
   
